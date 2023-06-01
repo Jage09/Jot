b6d8083969b3e65b8c1408663931036c201bb35c
Tanaphol Fowler <tanfow99@gmail.com>
2023-06-01 13:49:27 -0700

    added the skeleton functions that will be able to add, edit, remove and count the items in the list. As well as fetching all items and closing the edit box

---
+++ script.js
@@ -0,0 +1,31 @@
+var app = new function() {
+    this.el = document.getElementById("list_items");
+    this.list_items=[]
+
+    this.FetchAll = function() {
+
+    };
+
+    this.Add = function() {
+
+    };
+
+    this.Edit = function(item) {
+
+    };
+
+    this.Delete = function(item) {
+
+    };
+
+    this.Count = function(counter) {
+
+    };
+
+}
+
+app.FetchAll();
+
+function CloseInput() {
+    document.getElementById("edit-box").style.display="none";
+}
